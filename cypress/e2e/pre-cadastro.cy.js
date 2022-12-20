6ea0622918daacb666949f93c648f60de46bade7
Viviane Monteiro <vivianemonteiro@yahoo.com.br>
2022-12-20 11:22:55 -0300

    Inclusão dos testes de produtos

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -2,22 +2,49 @@
 import { faker } from '@faker-js/faker'
 
 describe('Funcionalidade Pré Cadastro', () => {
+    let emailFaker = faker.internet.email()
+    let nameFaker = faker.name.firstName()
+    let lastNameFaker = faker.name.lastName()
+    let password = 'Teste@vivi123'
+
 
     beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-
     });
-    
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
     it('Deve completar o pré cadastro com sucesso', () => {
-        cy.get('#reg_email').type(faker.internet.email())
-        cy.get('#reg_password').type('Teste@vivi123')
+
+        cy.get('#reg_email').type(emailFaker)
+        cy.get('#reg_password').type(password)
         cy.get(':nth-child(4) > .button').click()
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type(faker.name.firstName())
-        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('#account_first_name').type(nameFaker)
+        cy.get('#account_last_name').type(lastNameFaker)
         cy.get('.woocommerce-Button').click()
-        cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso.')
-
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
     });
+
+    it('Pré cadastro sem informar o e-mail', () => {
+        cy.get('#reg_password').type(password)
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'Erro: Informe um endereço de e-mail válido.')
+    });
+
+    it('Pré cadastro sem informar a senha', () => {
+        cy.get('#reg_email').type(emailFaker)
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'Erro: Digite a senha da conta.')
+    });
+
+    it('Pré cadastro sem informar e-mail e senha', () => {
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'Erro: Informe um endereço de e-mail válido.')
+    });
+
+
 
 });
---
+++ cypress/e2e/produtos.cy.js
@@ -0,0 +1,42 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade: Página de produtos', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar o primeiro produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+        .first()
+        .click()
+    });
+
+    it('Deve selecionar um produto pela posição na lista', () => {
+        cy.get('[class="product-block grid"]')
+        .eq(1)
+        .click()
+    });
+
+    it('Deve selecionar um produto da lista pelo nome', () => {
+        cy.get('[class="product-block grid"]')
+        .contains('Ariel Roll Sleeve Sweatshirt')
+        .click()
+    });
+
+    it('Deve adicionar um item ao carrinho', () => {
+        var quantidade = 5
+
+        cy.get('[class="product-block grid"]')
+        .contains('Ajax Full-Zip Sweatshirt')
+        .click()
+        cy.get('.button-variable-item-L').click()
+        cy.get('.button-variable-item-Red').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain' , quantidade)
+        cy.get('.woocommerce-message').should('contain' , quantidade +' × “Ajax Full-Zip Sweatshirt” foram adicionados no seu carrinho.')
+
+    });
+
+});
