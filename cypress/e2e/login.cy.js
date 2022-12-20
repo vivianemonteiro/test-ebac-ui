af8b798242c60ab2e482ad2f2c878683d65fd1bf
Viviane Monteiro <vivianemonteiro@yahoo.com.br>
2022-12-20 08:31:42 -0300

    Meu primeiro commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+
+context('Funcionalidade Login', () =>{
+
+    it('Deve fazer login com sucesso' , () =>{
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('vivianemonteiro@mailinator.com')
+        cy.get('#password').type('Teste@cypress1234')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.page-title').should('contain', 'Minha conta')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, vivianemonteiro (não é vivianemonteiro? Sair)')
+    })
+    
+    it('Deve exibir uma mensagem de erro ao inserir um usuário inválido' , () =>{
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('vivianemonteiro@teste.com')
+        cy.get('#password').type('Teste@cypress1234')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error > li').should('contain' ,  'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+    })
+
+    it('Deve exibir uma mensagem de erro ao inserir uma senha inválida' , () =>{
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('vivianemonteiro@mailinator.com')
+        cy.get('#password').type('Teste@1234')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should('contain' ,  'Erro: a senha fornecida para o e-mail vivianemonteiro@mailinator.com está incorreta. Perdeu a senha?')
+    })
+
+
+})
